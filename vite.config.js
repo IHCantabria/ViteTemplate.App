29fc4404e2fb7daae1240ccbd52e37100fc34333
Diego Elola Portilla <elolad@unican.es>
2022-09-13 08:59:57 +0200

    feat: localhost is now set as default instead of ip 127.0.0.1. Vite and Vue updated

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import viteCompression from "vite-plugin-compression";
+import dns from "dns";
 const zlib = require("zlib");
 const fs = require("fs");
+
+// Localhost instead of ip 127.0.0.1
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
